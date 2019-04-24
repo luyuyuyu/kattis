5a87c3845d99c121b6d5f01f5f2213800f4eaeb5
luyuyuyu <45277278+luyuyuyu@users.noreply.github.com>
2019-04-25 00:31:09 +0800

    Add files via upload

---
+++ mnist2class.cs
@@ -0,0 +1,16 @@
+using System;
+
+class mnist2class {
+	public static void Main() {
+		var count = 0;
+		while (count < 30) {
+			var count51 = 0;
+			while (count51 < 51) {
+				Console.Write("-1 ");
+				++count51;
+			}
+			Console.WriteLine();
+			++count;
+		}
+	}
+}
